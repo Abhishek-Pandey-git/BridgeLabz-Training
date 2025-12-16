f729949dce74c4511711a233bc83a07b9c7e42bc
Codekster <hbfabhi86@gmail.com>
2025-12-16 16:53:42 +0530

    [Abhishek Pandey] Add program to display welcome message

---
+++ gcr-codebase/program-prerequisites/WelcomeToBridgelabz.java
@@ -0,0 +1,7 @@
+// Program to print a welcome message
+public class WelcomeToBridgelabz {
+    
+    public static void main(String[] args) {
+        System.out.println("Welcome to Bridgelabz!");
+    }
+}
