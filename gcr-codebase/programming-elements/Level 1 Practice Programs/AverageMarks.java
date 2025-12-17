d54da47bd9af1cfc4a7e16433381db68053af0ff
Codekster <hbfabhi86@gmail.com>
2025-12-17 12:46:59 +0530

    [Abhishek Pandey] adding AverageMarks Program from Level 1 practice Program

---
+++ gcr-codebase/programming-elements/Level 1 Practice Programs/AverageMarks.java
@@ -0,0 +1,11 @@
+//package gcr-codebase.programming-elements.Level 1 Practice Programs;
+
+public class AverageMarks {
+    public static void main(String[] args){
+        //variables to store marks of three subjects
+        int marksInMaths=94, marksInPhysics=95, marksInChemistry=96;
+        double averageMarks=(marksInChemistry+marksInMaths+marksInPhysics)/3;
+        System.out.println("Sam’s average mark in PCM is "+averageMarks);
+
+    }
+}
