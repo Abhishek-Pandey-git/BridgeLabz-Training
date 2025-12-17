19c9110792e7a2bf100445e7821c68f5c7e31b46
Codekster <hbfabhi86@gmail.com>
2025-12-17 12:23:02 +0530

    [Abhishek Pandey] Add AgeOfHarry prgram from level 1 practice problem week1.

---
+++ gcr-codebase/programming-elements/Level 1 Practice Programs/AgeOfHarry.java
@@ -0,0 +1,9 @@
+
+public class AgeOfHarry {
+    public static void main(String[] args){
+        //Variables to store Year of birth and current year
+        int  yearOfBirth=2000, currentYear=2024;
+        int currentAge=currentYear-yearOfBirth;
+        System.out.println("Harry's age in "+currentYear+" is "+currentAge);
+    }
+}
