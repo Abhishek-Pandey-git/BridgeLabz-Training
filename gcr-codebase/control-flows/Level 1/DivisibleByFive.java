7049f324960928bbc939c15298f7df5b85d96d04
Codekster <hbfabhi86@gmail.com>
2025-12-18 12:10:35 +0530

    [Abhishek Pandey] Add Divisible by five program

---
+++ gcr-codebase/control-flows/Level 1/DivisibleByFive.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class DivisibleByFive {
+    public static void main(String[] args){
+        double number;
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        number=input.nextDouble();
+        if(number%5==0){
+            System.out.println(number+ " is divisible by 5");
+        }
+        else{
+            System.out.println(number+" is not divisible by 5");
+        }
+    }
+}
