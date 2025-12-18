b00922a99086aff6d8c624cd82a9c25f0eeda124
Codekster <hbfabhi86@gmail.com>
2025-12-18 16:00:48 +0530

    [Abhishek Pandey] Add Factors below hundred program

---
+++ gcr-codebase/control-flows/Level 2/FactorsBelowHundred.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+//Q11
+public class FactorsBelowHundred{
+	public static void main(String[] args){
+		double number;
+		Scanner input=new Scanner(System.in);
+		
+		//Taking input
+		while(true)
+		{
+			System.out.println("Enter a number");
+			number=input.nextDouble();
+			if(number<=0 || number>100 )
+			{
+				System.out.println("Number must be a positive and less than 100. Try again!!!");
+			}
+			else
+			{
+				break;
+			}
+		}
+		
+		for(int i=100;i>=1;i--){
+			if(number%i==0){
+				System.out.println(i);
+				continue;
+			}
+		}
+		
+	}
+}
