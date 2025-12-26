b03315080a60b779f645e5b775ca8a776d21cb24
Codekster <hbfabhi86@gmail.com>
2025-12-26 22:05:51 +0530

    [Abhishek Pandey]: Add final value after operations program from leetcode

---
+++ leet-code-codebase/FinalValueAfterOperations.java
@@ -0,0 +1,31 @@
+/*
+There is a programming language with only four operations and one variable X:
+
+++X and X++ increments the value of the variable X by 1.
+--X and X-- decrements the value of the variable X by 1.
+Initially, the value of X is 0.
+
+Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+*/
+
+public class FinalValueAfterOperations {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for (int i = 0; i < operations.length; i++) {
+            if (operations[i].equals("++X") || operations[i].equals("X++")) {
+                x++;
+            } else if (operations[i].equals("--X") || operations[i].equals("X--")) {
+                x--;
+            }
+        }
+        return x;
+    }
+
+    public static void main(String[] args) {
+        FinalValueAfterOperations solution = new FinalValueAfterOperations();
+        
+        String[] operations = {"--X", "X++", "X++"};
+        int result = solution.finalValueAfterOperations(operations);
+        System.out.println("Final value of X: " + result);
+    }
+}
