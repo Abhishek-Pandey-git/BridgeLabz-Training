d2c3219210ab44ae8a088719adbe8e33fd6c9b48
Codekster <hbfabhi86@gmail.com>
2025-12-26 22:08:55 +0530

    [Abhishek Pandey]: Add generate concatenation program from leetcode

---
+++ leet-code-codebase/GenerateConcatenation.java
@@ -0,0 +1,31 @@
+/*
+Given an integer array nums of length n, you want to create an array ans of length 2n where
+ ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+Specifically, ans is the concatenation of two nums arrays.
+
+Return the array ans.7
+*/
+
+public class GenerateConcatenation {
+    public static int[] getConcatenation(int[] nums) {
+        int[] ans=new int[2*nums.length];
+
+        for(int i=0;i<nums.length;i++){
+            ans[i]=nums[i];
+            ans[i+nums.length]=nums[i];
+        }
+
+        return ans;
+    }
+    public static void main(String[] args) {
+       int[] nums = {1, 2, 3};
+       int[] result = getConcatenation(nums);
+
+       // Print the resulting array
+       System.out.print("Concatenated array: ");
+       for (int num : result) {
+           System.out.print(num + " ");
+       }    
+    }
+}
