9d5ed52f54d5b1606dff04876cf4b84cb72bc294
Codekster <hbfabhi86@gmail.com>
2026-01-02 15:24:12 +0530

    [Abhishek Pandey]: Add Author.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritence/singleinheritence/Author.java
@@ -0,0 +1,20 @@
+package com.inheritence.singleinheritence;
+
+// Author is modeled as a subclass of Book for this exercise
+public class Author extends Book {
+    private String name;
+    private String bio;
+
+    public Author(String title, int publicationYear, String name, String bio) {
+        super(title, publicationYear);
+        this.name = name;
+        this.bio = bio;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Author Name: " + name);
+        System.out.println("Author Bio: " + bio);
+    }
+}
