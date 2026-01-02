187810d2ae6223d29948349680ac0a47f708400a
Codekster <hbfabhi86@gmail.com>
2026-01-02 15:32:51 +0530

    [Abhishek Pandey]: Add OnlineCourse.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritence/multilevelinheritence/OnlineCourse.java
@@ -0,0 +1,20 @@
+package com.inheritence.multilevelinheritence;
+
+// OnlineCourse adds platform and recorded flag
+public class OnlineCourse extends Course {
+    protected String platform;
+    protected boolean isRecorded;
+
+    public OnlineCourse(String courseName, int duration, String platform, boolean isRecorded) {
+        super(courseName, duration);
+        this.platform = platform;
+        this.isRecorded = isRecorded;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Platform: " + platform);
+        System.out.println("Recorded: " + (isRecorded ? "Yes" : "No"));
+    }
+}
