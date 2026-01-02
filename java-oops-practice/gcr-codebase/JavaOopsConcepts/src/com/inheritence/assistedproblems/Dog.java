4383bbcd649badfb995680cc1a920e809ad5bd01
Codekster <hbfabhi86@gmail.com>
2026-01-02 16:28:35 +0530

    [Abhishek Pandey]: Add Dog.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritence/assistedproblems/Dog.java
@@ -0,0 +1,13 @@
+package com.inheritence.assistedproblems;
+
+// Dog class extends Animal
+public class Dog extends Animal {
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " says Woof!");
+    }
+}
