61c454622b11d51b1586604fd345f5d06957031c
Codekster <hbfabhi86@gmail.com>
2026-01-02 16:27:15 +0530

    [Abhishek Pandey]: Add Animal.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritence/assistedproblems/Animal.java
@@ -0,0 +1,16 @@
+package com.inheritence.assistedproblems;
+
+// Animal class
+public class Animal {
+    String name;
+    int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
