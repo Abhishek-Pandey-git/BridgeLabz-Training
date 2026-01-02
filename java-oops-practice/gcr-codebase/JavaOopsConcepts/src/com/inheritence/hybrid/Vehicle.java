4e01495eec5e5dfface8cd5bef4882eb3bcf8a29
Codekster <hbfabhi86@gmail.com>
2026-01-02 15:41:05 +0530

    [Abhishek Pandey]: Add Vehicle.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritence/hybrid/Vehicle.java
@@ -0,0 +1,17 @@
+package com.inheritence.hybrid;
+
+// Basic vehicle superclass
+public class Vehicle {
+    protected String model;
+    protected int maxSpeed;
+
+    public Vehicle(String model, int maxSpeed) {
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void display() {
+        System.out.println("Model: " + model);
+        System.out.println("Max Speed: " + maxSpeed + " km/h");
+    }
+}
