0d41544cdc485f7a44a4c4f58949fda3def497a1
Codekster <hbfabhi86@gmail.com>
2026-01-02 16:29:41 +0530

    [Abhishek Pandey]: Add Motorcycle.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritence/assistedproblems/Motorcycle.java
@@ -0,0 +1,17 @@
+package com.inheritence.assistedproblems;
+
+// Motorcycle class extends Vehicle
+public class Motorcycle extends Vehicle {
+    boolean hasSidecar;
+
+    public Motorcycle(int maxSpeed, String fuelType, boolean hasSidecar) {
+        super(maxSpeed, fuelType);
+        this.hasSidecar = hasSidecar;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Has Sidecar: " + hasSidecar);
+    }
+}
