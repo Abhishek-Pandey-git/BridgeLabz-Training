1afa5d817c9378fae6e3fef3819319539090a77e
Codekster <hbfabhi86@gmail.com>
2026-01-03 12:33:02 +0530

    [Abhishek Pandey]:Add Doctor.java

---
+++ java-oops-practice/scenario-based-questions/scenarioBasedQuestions/src/com/scenariobasedquestion/hospitalscenario/Doctor.java
@@ -0,0 +1,20 @@
+package com.scenariobasedquestion.hospitalscenario;
+
+// Doctor class
+public class Doctor {
+    private String id;
+    private String name;
+    private String specialization;
+
+    public Doctor(String id, String name, String specialization) {
+        this.id = id;
+        this.name = name;
+        this.specialization = specialization;
+    }
+
+    public void displayInfo() {
+        System.out.println("Doctor ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Specialization: " + specialization);
+    }
+}
