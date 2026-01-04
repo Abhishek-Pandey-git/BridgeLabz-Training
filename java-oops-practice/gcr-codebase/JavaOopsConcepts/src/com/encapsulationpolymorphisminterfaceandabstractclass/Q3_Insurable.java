0350aa65d151797e6bed376a02f4b3e2a385e1b7
Codekster <hbfabhi86@gmail.com>
2026-01-04 22:29:50 +0530

    [Abhishek Pandey]:Add Q3_Insurable.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationpolymorphisminterfaceandabstractclass/Q3_Insurable.java
@@ -0,0 +1,8 @@
+package com.encapsulationpolymorphisminterfaceandabstractclass;
+
+//insurable interface
+public interface Q3_Insurable {
+    
+    double calculateInsurance();
+    String getInsuranceDetails();
+}
