ac75d0dd470af9d9ff771c319badc431c5aa9280
Codekster <hbfabhi86@gmail.com>
2026-01-04 22:32:23 +0530

    [Abhishek Pandey]:Add Q4_Loanable.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationpolymorphisminterfaceandabstractclass/Q4_Loanable.java
@@ -0,0 +1,8 @@
+package com.encapsulationpolymorphisminterfaceandabstractclass;
+
+//loanable interface
+public interface Q4_Loanable {
+    
+    void applyForLoan(double amount);
+    boolean calculateLoanEligibility();
+}
