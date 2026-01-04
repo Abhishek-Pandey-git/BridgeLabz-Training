b0b37d6137698bf75bbf8f7c7c3807803a8e7440
Codekster <hbfabhi86@gmail.com>
2026-01-04 22:35:19 +0530

    [Abhishek Pandey]:Add Q6_Discountable.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationpolymorphisminterfaceandabstractclass/Q6_Discountable.java
@@ -0,0 +1,8 @@
+package com.encapsulationpolymorphisminterfaceandabstractclass;
+
+//discountable interface
+public interface Q6_Discountable {
+    
+    double applyDiscount();
+    String getDiscountDetails();
+}
