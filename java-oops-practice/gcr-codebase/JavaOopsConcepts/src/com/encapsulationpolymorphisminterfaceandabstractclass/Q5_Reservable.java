6d5f0dcb4b8bef006cd7fc7aa8e70982414a639e
Codekster <hbfabhi86@gmail.com>
2026-01-04 22:34:57 +0530

    [Abhishek Pandey]:Add Q5_Reservable.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationpolymorphisminterfaceandabstractclass/Q5_Reservable.java
@@ -0,0 +1,8 @@
+package com.encapsulationpolymorphisminterfaceandabstractclass;
+
+//reservable interface
+public interface Q5_Reservable {
+    
+    void reserveItem(String borrowerName);
+    boolean checkAvailability();
+}
