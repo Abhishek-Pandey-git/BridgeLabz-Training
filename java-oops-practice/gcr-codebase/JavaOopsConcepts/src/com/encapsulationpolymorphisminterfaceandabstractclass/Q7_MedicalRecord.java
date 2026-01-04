9402b45e82c4ee5bfdd4af4cf30c9bc980b37ae4
Codekster <hbfabhi86@gmail.com>
2026-01-04 22:37:54 +0530

    [Abhishek Pandey]:Add Q7_MedicalRecord.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationpolymorphisminterfaceandabstractclass/Q7_MedicalRecord.java
@@ -0,0 +1,8 @@
+package com.encapsulationpolymorphisminterfaceandabstractclass;
+
+//medical record interface
+public interface Q7_MedicalRecord {
+    
+    void addRecord(String record);
+    void viewRecords();
+}
