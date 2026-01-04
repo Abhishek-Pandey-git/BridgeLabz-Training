8b557a4f3cb0cfe33450f43ebf00eb913b9e7207
Codekster <hbfabhi86@gmail.com>
2026-01-04 22:39:00 +0530

    [Abhishek Pandey]:Add Q8_GPS.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationpolymorphisminterfaceandabstractclass/Q8_GPS.java
@@ -0,0 +1,8 @@
+package com.encapsulationpolymorphisminterfaceandabstractclass;
+
+//gps interface
+public interface Q8_GPS {
+    
+    String getCurrentLocation();
+    void updateLocation(String newLocation);
+}
