b1026b993b90d1212552f6d7a2f26a5020bad970
Codekster <hbfabhi86@gmail.com>
2026-01-04 22:19:13 +0530

    [Abhishek Pandey]:Add Q1_Department.java

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationpolymorphisminterfaceandabstractclass/Q1_Department.java
@@ -0,0 +1,8 @@
+package com.encapsulationpolymorphisminterfaceandabstractclass;
+
+//interface for department operations
+public interface Q1_Department {
+    
+    void assignDepartment(String deptName);
+    String getDepartmentDetails();
+}
